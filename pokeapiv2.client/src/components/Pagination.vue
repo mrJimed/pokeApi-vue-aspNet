938c149314028fb5dbce5d3eaf72069b9f4f5285
Danil <danil0316@mail.ru>
2024-02-09 17:03:52 +0500

    add Pagination

---
+++ pokeapiv2.client/src/components/Pagination.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  pageCounts: Number,
+  currentPage: Number,
+});
+
+const emits = defineEmits(["nextPageClick", "prevPageClick"]);
+</script>
+
+<template>
+  <div
+    class="flex gap-4 border border-slate-400 w-fit rounded-md overflow-hidden select-none items-center"
+  >
+    <button
+      class="pl-2 pr-2 py-2 bg-slate-200 transition hover:bg-slate-300 active:bg-slate-400 disabled:bg-red-400 disabled:text-white"
+      type="button"
+      @click="emits('prevPageClick')"
+      :disabled="currentPage == 1"
+    >
+      Пред.
+    </button>
+
+    <span>{{ currentPage }} из {{ pageCounts }}</span>
+
+    <button
+      class="pl-2 pr-2 py-2 bg-slate-200 transition hover:bg-slate-300 active:bg-slate-400 disabled:bg-red-400 disabled:text-white"
+      type="button"
+      @click="emits('nextPageClick')"
+      :disabled="currentPage == pageCounts"
+    >
+      След.
+    </button>
+  </div>
+</template>
--- pokeapiv2.client/src/pages/Home.vue
+++ pokeapiv2.client/src/pages/Home.vue
@@ -1,26 +1,60 @@
 <script setup>
 import Card from "@/components/Card.vue";
+import Pagination from "@/components/Pagination.vue";
 
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { getAllPokemonsAsync } from "../services/pokemonService.js";
 
-const pokemons = ref([]);
+const allPokemons = ref([]);
+const pagePokemons = ref([]);
+const currentPage = ref(1);
+const countPageItems = ref(12);
+
+const pageCounts = computed(() =>
+  Math.ceil(allPokemons.value.length / countPageItems.value)
+);
 
 onMounted(async () => {
-  pokemons.value = await getAllPokemonsAsync();
+  allPokemons.value = await getAllPokemonsAsync();
+  pagePokemons.value = allPokemons.value.slice(0, countPageItems.value);
 });
+
+// watch on change currentPage variable
+watch(currentPage, () => {
+  const startIndex = (currentPage.value - 1) * countPageItems.value;
+  const endIndex = currentPage.value * countPageItems.value;
+  pagePokemons.value = allPokemons.value.slice(startIndex, endIndex);
+});
+
+// Pagination functions
+function nextPageClick() {
+  currentPage.value++;
+}
+
+function prevPageClick() {
+  currentPage.value--;
+}
 </script>
 
 <template>
   <div class="mb-5">
-    <h2 class="text-2xl font-bold">Список покемонов ({{ pokemons.length }})</h2>
+    <h2 class="text-2xl font-bold">
+      Список покемонов ({{ allPokemons.length }})
+    </h2>
   </div>
 
-  <div class="grid grid-cols-4 gap-y-6 place-items-center">
+  <div class="grid grid-cols-4 gap-y-6 place-items-center mb-10">
     <Card
       :id="pokemon.id"
-      v-for="pokemon in pokemons.slice(0, 12)"
+      v-for="pokemon in pagePokemons"
       :key="pokemon.id"
     ></Card>
   </div>
+
+  <Pagination
+    :pageCounts="pageCounts"
+    :currentPage="currentPage"
+    @nextPageClick="nextPageClick"
+    @prevPageClick="prevPageClick"
+  ></Pagination>
 </template>
