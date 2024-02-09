b1ad2e94e7e09d88fe41dfff2d7fff2fd89ada40
Danil <danil0316@mail.ru>
2024-02-09 17:17:31 +0500

    add icons

--- pokeapiv2.client/src/components/Pagination.vue
+++ pokeapiv2.client/src/components/Pagination.vue
@@ -12,23 +12,23 @@ const emits = defineEmits(["nextPageClick", "prevPageClick"]);
     class="flex gap-4 border border-slate-400 w-fit rounded-md overflow-hidden select-none items-center"
   >
     <button
-      class="pl-2 pr-2 py-2 bg-slate-200 transition hover:bg-slate-300 active:bg-slate-400 disabled:bg-red-400 disabled:text-white"
+      class="px-3 py-2 bg-slate-200 transition hover:bg-slate-300 active:bg-slate-400 disabled:bg-red-400 disabled:text-white"
       type="button"
       @click="emits('prevPageClick')"
       :disabled="currentPage == 1"
     >
-      Пред.
+      <i class="fa-solid fa-chevron-left"></i>
     </button>
 
     <span>{{ currentPage }} из {{ pageCounts }}</span>
 
     <button
-      class="pl-2 pr-2 py-2 bg-slate-200 transition hover:bg-slate-300 active:bg-slate-400 disabled:bg-red-400 disabled:text-white"
+      class="px-3 py-2 bg-slate-200 transition hover:bg-slate-300 active:bg-slate-400 disabled:bg-red-400 disabled:text-white"
       type="button"
       @click="emits('nextPageClick')"
       :disabled="currentPage == pageCounts"
     >
-      След.
+      <i class="fa-solid fa-chevron-right"></i>
     </button>
   </div>
 </template>
