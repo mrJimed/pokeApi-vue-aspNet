a3cf63a4d1d197bcd2a1cc6c8c9e575f2e0ec22e
Danil <danil0316@mail.ru>
2024-02-10 20:06:18 +0500

    update App.vue

--- pokeapiv2.client/src/App.vue
+++ pokeapiv2.client/src/App.vue
@@ -5,7 +5,7 @@ import Header from "./components/Header.vue";
 <template>
   <Header></Header>
 
-  <div class="w-11/12 mx-auto mt-5">
+  <div class="w-11/12 mx-auto mt-5 mb-7">
     <router-view></router-view>
   </div>
 </template>
