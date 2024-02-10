db94a6a752d8d97d4902c4903326fdf3546c1842
Danil <danil0316@mail.ru>
2024-02-10 20:03:59 +0500

    add header

--- pokeapiv2.client/src/App.vue
+++ pokeapiv2.client/src/App.vue
@@ -1,5 +1,11 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
 <template>
-  <div class="w-11/12 mx-auto">
+  <Header></Header>
+
+  <div class="w-11/12 mx-auto mt-5">
     <router-view></router-view>
   </div>
 </template>
---
+++ pokeapiv2.client/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="bg-red-600 py-2">
+        <div class="w-11/12 mx-auto">
+            <img class="w-40 h-auto" src="/logo.png" alt="Pokemon" />
+        </div>
+    </header>
+</template>
