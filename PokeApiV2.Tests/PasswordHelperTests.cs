5eae2ecf19b44dc9c85d5d2ee67be62d0d0273b4
Danil <danil0316@mail.ru>
2024-02-11 19:17:33 +0500

    add PasswordHelperTests

---
+++ PokeApiV2.Tests/PasswordHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using PokeApiV2.Server.Services.Classes;
+using PokeApiV2.Server.Services.Interfaces;
+using Xunit;
+
+namespace PokeApiV2.Tests
+{
+    public class PasswordHelperTests
+    {
+        private IPasswordHelper passwordHelper;
+
+        public PasswordHelperTests()
+        {
+            passwordHelper = new PasswordHelper();
+        }
+
+        [Fact]
+        public void CreateHashPasswordTest()
+        {
+            var password1 = "123456";
+            var password2 = "password";
+            var hashPasswordData = passwordHelper.CreateHashPassword(password1);
+
+            Assert.Equal(hashPasswordData.Item1, passwordHelper.CreateHashPassword(password1, hashPasswordData.Item2));
+            Assert.NotEqual(hashPasswordData.Item1, passwordHelper.CreateHashPassword(password2, hashPasswordData.Item2));
+        }
+    }
+}
