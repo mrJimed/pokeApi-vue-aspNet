c3680b86e1d64a94596b3be05eae473135e7cd82
Danil <danil0316@mail.ru>
2024-02-11 17:28:59 +0500

    update header

--- pokeapiv2.client/src/components/Header.vue
+++ pokeapiv2.client/src/components/Header.vue
@@ -1,7 +1,13 @@
 <template>
-    <header class="bg-red-600 py-2">
-        <div class="w-11/12 mx-auto">
-            <img class="w-40 h-auto" src="/logo.png" alt="Pokemon" />
-        </div>
-    </header>
+  <header class="bg-slate-100 shadow-md py-3">
+    <div class="w-11/12 mx-auto flex items-center justify-between">
+        <router-link to="/">
+          <img class="w-40 h-auto" src="/logo.png" alt="Pokemon" />
+        </router-link>
+
+      <router-link to="/login" title="Вход">
+        <i class="fa-solid fa-right-to-bracket text-xl"></i>
+      </router-link>
+    </div>
+  </header>
 </template>
